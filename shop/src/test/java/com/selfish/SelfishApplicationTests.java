6c92e39b5fc6ebfe4cdd4c297977876da07f08a1
BHGoo <gbh1055@naver.com>
2022-12-22 14:22:15 +0900

    selfish first commit

---
+++ shop/src/test/java/com/selfish/SelfishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
